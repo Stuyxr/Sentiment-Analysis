824ad0667010fdc56ad903b9cfa649e2de779604
ruhuang <ruhuang2001@gmail.com>
2023-05-05 14:49:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 from load_data import get_data
 from sentiment_analysis import SentimentAnalysisModule
 
@@ -14,4 +13,6 @@ input_data = torch.autograd.Variable(test_dataset).to(device)
 output = net(input_data).cpu()
 
 _, predicted = torch.max(output[-1, :, :], 1)
-np.savetxt("predicted_results.csv", predicted.numpy(), delimiter=",", fmt="%d")
+with open('1170300418.csv', 'a') as f:
+    for i in range(len(predicted)):
+        f.write(f'{i}, {predicted[i]}\r\n')
